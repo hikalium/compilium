9afffaf4bc0033dcb1e1ced3a4284f9543d2605c
hikalium <hikalium@hikalium.com>
2018-11-05 23:57:40 +0900

    Rename test_result to test_expr_result for adding more test types

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-function test_result {
+function test_expr_result {
   input="$1"
   expected="$2"
   ./compilium --target-os `uname` "{return $input;}" > out.S
@@ -15,142 +15,142 @@ function test_result {
 }
 
 # Integer literal
-test_result '0' 0
-test_result '1' 1
-test_result '17' 17
-test_result '017' 15
+test_expr_result '0' 0
+test_expr_result '1' 1
+test_expr_result '17' 17
+test_expr_result '017' 15
 
 # Non-printable
-test_result ' 0 ' 0
+test_expr_result ' 0 ' 0
 
 # Unary Prefix
-test_result '+0' 0
-test_result '+ +1' 1
-test_result '- -17' 17
-test_result '1 - -2' 3
+test_expr_result '+0' 0
+test_expr_result '+ +1' 1
+test_expr_result '- -17' 17
+test_expr_result '1 - -2' 3
 
-test_result '~10 & 15' 5
-test_result '~5 & 15' 10
+test_expr_result '~10 & 15' 5
+test_expr_result '~5 & 15' 10
 
-test_result '!0' 1
-test_result '!1' 0
-test_result '!2' 0
+test_expr_result '!0' 1
+test_expr_result '!1' 0
+test_expr_result '!2' 0
 
 # Multiplicative
-test_result '3 * 4' 12
-test_result '3 * 4 * 5' 60
-test_result '365 / 7' 52
-test_result '365 / 7 / 8' 6
-test_result '365 % 7' 1
-test_result '365 % 7 % 8' 1
+test_expr_result '3 * 4' 12
+test_expr_result '3 * 4 * 5' 60
+test_expr_result '365 / 7' 52
+test_expr_result '365 / 7 / 8' 6
+test_expr_result '365 % 7' 1
+test_expr_result '365 % 7 % 8' 1
 
 # Additive
-test_result '100 + 7' 107
-test_result '3 + 5 + 7 + 9' 24
-test_result '100 - 7' 93
-test_result '1 + 3 - 5 - 7 + 9' 1
+test_expr_result '100 + 7' 107
+test_expr_result '3 + 5 + 7 + 9' 24
+test_expr_result '100 - 7' 93
+test_expr_result '1 + 3 - 5 - 7 + 9' 1
 
 # Shift
-test_result '3 << 2' 12
-test_result '3 >> 2' 0
-test_result '17 >> 2' 4
-test_result '17 >> 2 >> 1' 2
+test_expr_result '3 << 2' 12
+test_expr_result '3 >> 2' 0
+test_expr_result '17 >> 2' 4
+test_expr_result '17 >> 2 >> 1' 2
 
 # Relational
-test_result '3 < 5' 1
-test_result '5 < 3' 0
-test_result '7 < 7' 0
+test_expr_result '3 < 5' 1
+test_expr_result '5 < 3' 0
+test_expr_result '7 < 7' 0
 
-test_result '3 <= 5' 1
-test_result '5 <= 3' 0
-test_result '7 <= 7' 1
+test_expr_result '3 <= 5' 1
+test_expr_result '5 <= 3' 0
+test_expr_result '7 <= 7' 1
 
-test_result '3 > 5' 0
-test_result '5 > 3' 1
-test_result '7 > 7' 0
+test_expr_result '3 > 5' 0
+test_expr_result '5 > 3' 1
+test_expr_result '7 > 7' 0
 
-test_result '3 >= 5' 0
-test_result '5 >= 3' 1
-test_result '7 >= 7' 1
+test_expr_result '3 >= 5' 0
+test_expr_result '5 >= 3' 1
+test_expr_result '7 >= 7' 1
 
 # Equality
-test_result '3 == 5' 0
-test_result '5 == 3' 0
-test_result '7 == 7' 1
+test_expr_result '3 == 5' 0
+test_expr_result '5 == 3' 0
+test_expr_result '7 == 7' 1
 
-test_result '3 != 5' 1
-test_result '5 != 3' 1
-test_result '7 != 7' 0
+test_expr_result '3 != 5' 1
+test_expr_result '5 != 3' 1
+test_expr_result '7 != 7' 0
 
 # Bitwise logical
-test_result '0 & 0' 0
-test_result '0 & 1' 0
-test_result '1 & 0' 0
-test_result '1 & 1' 1
+test_expr_result '0 & 0' 0
+test_expr_result '0 & 1' 0
+test_expr_result '1 & 0' 0
+test_expr_result '1 & 1' 1
 
-test_result '10 & 6' 2
+test_expr_result '10 & 6' 2
 
-test_result '0 ^ 0' 0
-test_result '0 ^ 1' 1
-test_result '1 ^ 0' 1
-test_result '1 ^ 1' 0
+test_expr_result '0 ^ 0' 0
+test_expr_result '0 ^ 1' 1
+test_expr_result '1 ^ 0' 1
+test_expr_result '1 ^ 1' 0
 
-test_result '10 ^ 6' 12
+test_expr_result '10 ^ 6' 12
 
-test_result '0 | 0' 0
-test_result '0 | 1' 1
-test_result '1 | 0' 1
-test_result '1 | 1' 1
+test_expr_result '0 | 0' 0
+test_expr_result '0 | 1' 1
+test_expr_result '1 | 0' 1
+test_expr_result '1 | 1' 1
 
-test_result '10 | 6' 14
+test_expr_result '10 | 6' 14
 
 # Boolean logical
-test_result '0 && 0' 0
-test_result '0 && 1' 0
-test_result '1 && 0' 0
-test_result '1 && 1' 1
+test_expr_result '0 && 0' 0
+test_expr_result '0 && 1' 0
+test_expr_result '1 && 0' 0
+test_expr_result '1 && 1' 1
 
-test_result '10 && 6' 1
+test_expr_result '10 && 6' 1
 
-test_result '0 || 0' 0
-test_result '0 || 1' 1
-test_result '1 || 0' 1
-test_result '1 || 1' 1
+test_expr_result '0 || 0' 0
+test_expr_result '0 || 1' 1
+test_expr_result '1 || 0' 1
+test_expr_result '1 || 1' 1
 
-test_result '10 || 6' 1
+test_expr_result '10 || 6' 1
 
-test_result '0 && 0 || 0' 0
-test_result '0 && 0 || 1' 1
-test_result '0 && 1 || 0' 0
-test_result '0 && 1 || 1' 1
-test_result '1 && 0 || 0' 0
-test_result '1 && 0 || 1' 1
-test_result '1 && 1 || 0' 1
-test_result '1 && 1 || 1' 1
+test_expr_result '0 && 0 || 0' 0
+test_expr_result '0 && 0 || 1' 1
+test_expr_result '0 && 1 || 0' 0
+test_expr_result '0 && 1 || 1' 1
+test_expr_result '1 && 0 || 0' 0
+test_expr_result '1 && 0 || 1' 1
+test_expr_result '1 && 1 || 0' 1
+test_expr_result '1 && 1 || 1' 1
 
-test_result '0 || 0 && 0' 0
-test_result '0 || 0 && 1' 0
-test_result '0 || 1 && 0' 0
-test_result '0 || 1 && 1' 1
-test_result '1 || 0 && 0' 1
-test_result '1 || 0 && 1' 1
-test_result '1 || 1 && 0' 1
-test_result '1 || 1 && 1' 1
+test_expr_result '0 || 0 && 0' 0
+test_expr_result '0 || 0 && 1' 0
+test_expr_result '0 || 1 && 0' 0
+test_expr_result '0 || 1 && 1' 1
+test_expr_result '1 || 0 && 0' 1
+test_expr_result '1 || 0 && 1' 1
+test_expr_result '1 || 1 && 0' 1
+test_expr_result '1 || 1 && 1' 1
 
 # conditional
-test_result '0 ? 3 : 5' 5
-test_result '1 ? 3 : 5' 3
-test_result '2 ? 3 : 5' 3
+test_expr_result '0 ? 3 : 5' 5
+test_expr_result '1 ? 3 : 5' 3
+test_expr_result '2 ? 3 : 5' 3
 
 # comma
-test_result '2, 3' 3
-test_result '2 * 3, 5 + 7' 12
+test_expr_result '2, 3' 3
+test_expr_result '2 * 3, 5 + 7' 12
 
 # Mixed priority
-test_result '3 * 4 + 5' 17
-test_result '3 + 4 * 5' 23
-test_result '3 + 4 * 5 - 9' 14
-test_result '3 + 14 / 2' 10
-test_result '12 + 17 % 7' 15
-test_result '1 + 2 << 3' 24
-test_result '-3 * -4 + -5' 7
+test_expr_result '3 * 4 + 5' 17
+test_expr_result '3 + 4 * 5' 23
+test_expr_result '3 + 4 * 5 - 9' 14
+test_expr_result '3 + 14 / 2' 10
+test_expr_result '12 + 17 % 7' 15
+test_expr_result '1 + 2 << 3' 24
+test_expr_result '-3 * -4 + -5' 7
